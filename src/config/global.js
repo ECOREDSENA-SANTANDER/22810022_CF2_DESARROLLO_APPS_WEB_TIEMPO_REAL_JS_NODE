7e835015264721655105e4107fa64a989611468a
lizmanchego <karinamanchego0416@gmail.com>
2026-09-15 09:40:59 -0500

    ADD CFA + Material ZIP

--- src/config/global.js
+++ src/config/global.js
@@ -126,7 +126,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/22810022_CF2_DU.pdf',
       },
       {
         icono: 'fas fa-download',
